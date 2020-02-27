b13a2473ff34035182eded03f4f0e6b61623f7b4
juanzarama98 <juanzarama00@gmail.com>
2020-02-26 21:34:07 -0500

    Arreglos Multidimencionales

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,39 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*ruta para arreglos 
+multidimensionales*/
+
+route::get('arreglom', function(){ 
+    $paises = [ "Colombia"=> [
+                                "Capital" => "Bogotá",
+                                "Moneda" => "Peso",
+                                "Población" => 50
+                             ],
+                "Perú" => [
+                                "Capital" => "Lima",
+                                "Moneda" => "Sol",
+                                "Población" => 32                               
+                          ],
+                "Bolivia" =>[
+                                "Capital" => "Lima",
+                                "Moneda" => "Sol",
+                                "Población" => 12
+                            ]
+                ];
+/*
+    echo "<pre>";            
+    var_dump($paises);
+    echo "</pre>";
+*/
+    //Recorrer el arreglo países
+    foreach($paises as $pais => $informacion ){
+        echo"<h1> $pais</h1>";
+        echo $informacion["Capital"]. "<br/>";
+        echo"<hr />";
+    }
+    
+});
+
+
